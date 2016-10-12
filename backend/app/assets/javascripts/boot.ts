fc6a907b07979d4644a50c2f61bf207f6378278b
Dane <dane160290@yahoo.de>
2016-10-12 21:09:43 +0200

    remove all hero shit and let first directive do its work

--- backend/app/assets/javascripts/boot.ts
+++ backend/app/assets/javascripts/boot.ts
@@ -1,9 +1,11 @@
+/// <reference path="../../../node_modules/angular2/typings/browser.d.ts" />
+
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HeroService} from './hero.service';
+import {PlayerService} from './player.service';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
-  HeroService
+  PlayerService
 ]);
