45101e6a3ca5124714fd781786eea11e56ebc184
Dane <dane160290@yahoo.de>
2016-10-13 20:44:01 +0200

    show all players working, now navigation concept

---
+++ backend/app/assets/javascripts/service/player.service.ts
@@ -0,0 +1,40 @@
+import {Player} from '../data/player';
+import {Injectable} from 'angular2/core';
+import { Headers, Http } from 'angular2/http';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class PlayerService {
+  private playerUrl = 'getAllPlayer';
+
+  constructor(private http: Http){}
+
+  getPlayers() {
+    var PLAYERS: Player[] = [
+      {"id": 11, "firstName": "Kian", "lastName": "Aragian", "ttr":1660, paid: true, sex:'m'},
+      {"id": 12, "firstName": "Benjamin", "lastName": "Bauermeister", "ttr":1660, paid: true, sex:'m'},
+      {"id": 13, "firstName": "Sebastian", "lastName": "Sakmann", "ttr":1660, paid: true, sex:'m'},
+      {"id": 14, "firstName": "Rainald", "lastName": "Knaup", "ttr":1660, paid: true, sex:'m'},
+      {"id": 15, "firstName": "Jonas", "lastName": "Fürst", "ttr":1660, paid: true, sex:'m'}
+
+      ];
+    return PLAYERS;
+  }
+
+  getAllPlayers(): Promise<Player[]>{
+    return this.http.get(this.playerUrl)
+               .toPromise()
+               .then(response => {
+                 console.log(response.json());
+                 return response.json() as Player[];
+                })
+               .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+}
