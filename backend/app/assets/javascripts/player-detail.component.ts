034456bdd801cf6b973a612db9a046c04774207f
Dane <dane160290@yahoo.de>
2016-11-06 18:22:52 +0100

    remove client errors

--- backend/app/assets/javascripts/player-detail.component.ts
+++ backend/app/assets/javascripts/player-detail.component.ts
@@ -1,24 +1,11 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {Player} from "./data/player";
 
 @Component({
   selector: "my-player-detail",
-  template: `
-    <div *ngIf="player">
-      <h2>{{player.firstName}} details!</h2>
-      <div><label>id: </label>{{player.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="player.firstName" placeholder="firstName"/>
-      </div>
-       <div>
-        <label>lastName: </label>
-        <input [(ngModel)]="player.lastName" placeholder="lastName"/>
-      </div>
-    </div>
-  `,
-  inputs: ["player"]
+  templateUrl : "assets/javascripts/view/playerDetail.html"
 })
 export class PlayerDetailComponent{
-  public player:Player;
+  @Input() player:Player;
+
 }
