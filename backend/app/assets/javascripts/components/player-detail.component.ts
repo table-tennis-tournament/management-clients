177a9b7bbbdbda30b5b58b56f8f0b699d51a7588
Dane <dane160290@yahoo.de>
2016-11-20 17:04:41 +0100

    new component for table detail

--- backend/app/assets/javascripts/components/player-detail.component.ts
+++ backend/app/assets/javascripts/components/player-detail.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input} from "@angular/core";
 import {Player} from "../data/player";
 
+
 @Component({
   templateUrl : "assets/javascripts/view/playerDetail.html"
 })
 export class PlayerDetailComponent{
+  
   @Input() player:Player;
-
 }
