ef0e01b4551bf8d184779980c777859e7374552b
Dane <dane160290@yahoo.de>
2016-11-22 21:13:06 +0100

    optical improvements for table view. Some css styling => thanks to google

--- app/assets/javascripts/components/table.component.ts
+++ app/assets/javascripts/components/table.component.ts
@@ -11,22 +11,44 @@ export class TableComponent{
     public firstOpponent: string;
     public secondOpponent: string;
     public bgColor: string;
+    public tableNumber: string;
+    public firstClubName: string;
+    public secondClubName:string;
+    public stage: string;
 
+    public colorArray: string[] = [];
+    
     _match: Match;
     get match(): Match {
-    return this._match;
-}
+        return this._match;
+    }
 
     @Input("match")
     set match(value: Match){
         console.log(value);
         this._match = value;
-        this.firstOpponent = "Fritz Walter";
+        this._match.stage = "Achtelfinale";
+        this.firstOpponent = "Fritz Walter-Maier";
         this.secondOpponent = "Walter Maier";
-        this.bgColor = "orange";
+        this.bgColor = this.colorArray[this.getRandomInt(1, 6)];
+        this.tableNumber = this.getRandomInt(1, 25);
+        this.firstClubName = "(ASV Grünwettersbach)";
+        this.secondClubName = "(TTV Ettlingen)";
     } 
 
     constructor(){
         console.log(this.match);
+        this.colorArray[1] = "indigo";
+        this.colorArray[2] = "blue";
+        this.colorArray[3] = "red";
+        this.colorArray[4] = "deep-orange";
+        this.colorArray[5] = "brown";
+        this.colorArray[6] = "deep-purple";
+    }
+
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 }
