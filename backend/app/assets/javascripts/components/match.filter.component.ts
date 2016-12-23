8275740e927682af4451fdcf5d829783f2fbe6af
Dane <dane160290@yahoo.de>
2016-12-23 10:17:42 +0100

    first implementations

---
+++ backend/app/assets/javascripts/components/match.filter.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from "@angular/core";
+import {Match} from "../data/match";
+
+@Component({
+    selector: "match-filter",
+    templateUrl : "assets/javascripts/views/match.filter.component.html"
+})
+export class MatchFilterComponent{
+}
