34f5c54f47a2277bcdc76e16f42f6b036cf851bd
Dane <dane160290@yahoo.de>
2016-12-24 14:26:23 +0100

    angular2-materialize is running, first implementation for supervisor-view is working with filter object

--- backend/app/assets/javascripts/components/match.filter.component.ts
+++ backend/app/assets/javascripts/components/match.filter.component.ts
@@ -1,9 +1,17 @@
 import {Component, Input} from "@angular/core";
-import {Match} from "../data/match";
+import {FilterCriteria} from "../data/filterCriteria";
 
 @Component({
     selector: "match-filter",
     templateUrl : "assets/javascripts/views/match.filter.component.html"
 })
 export class MatchFilterComponent{
+
+    public filterCriterias: FilterCriteria[];
+
+    constructor(){
+        this.filterCriterias = [];
+        this.filterCriterias[0] = new FilterCriteria("discipline", 1, 1);
+        this.filterCriterias[1] = new FilterCriteria("discipline", 1, 1);
+    }
 }
