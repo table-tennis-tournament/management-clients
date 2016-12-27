d1d2b3664a4df4ebd8368277e4edb7510e173dba
Dane <dane160290@yahoo.de>
2016-12-27 16:33:51 +0100

    components are working

--- app/assets/javascripts/components/discipline.view.component.ts
+++ app/assets/javascripts/components/discipline.view.component.ts
@@ -8,22 +8,24 @@ import {DisciplineGroup} from "../data/discipline.group"
 })
 export class DisciplineViewComponent{
     public tabs: DisciplineTab[];
+
+    public rowCount: number
     
     constructor(){
         this.tabs = [];
         this.tabs[0] = new DisciplineTab(1, "Herren B");
-        this.tabs[0].groups = this.getDisciplineGroups();
+        this.tabs[0].groups = this.getDisciplineGroups(27);
         this.tabs[1] = new DisciplineTab(2, "Herren C");
-         this.tabs[1].groups = this.getDisciplineGroups();
+         this.tabs[1].groups = this.getDisciplineGroups(5);
         this.tabs[2] = new DisciplineTab(3, "Herren D");
-         this.tabs[3].groups = this.getDisciplineGroups();
+         this.tabs[2].groups = this.getDisciplineGroups(2);
         this.tabs[3] = new DisciplineTab(4, "Herren E");
-         this.tabs[3].groups = this.getDisciplineGroups();
+         this.tabs[3].groups = this.getDisciplineGroups(22);
     }
 
-    getDisciplineGroups(): DisciplineGroup[]{
-        var result : DisciplineGroup[] = [];
-        for(var i=0; i< 20; i++){
+    getDisciplineGroups(count: number): DisciplineGroup[]{
+        var result: DisciplineGroup[] = [];
+        for(var i=0; i< count; i++){
             result[i] = new DisciplineGroup();
         }
         return result;
--- app/assets/javascripts/data/discipline.tab.ts
+++ app/assets/javascripts/data/discipline.tab.ts
@@ -6,6 +6,10 @@ export class DisciplineTab{
     groups: DisciplineGroup[];
     assignedTables: number[];
 
+    get rowCount(): number[] {
+        return Array.from(Array(Math.ceil(this.groups.length / 4)).keys());
+    }
+
     constructor(id: number, name: string){
         this.id = id;
         this.name = name;
