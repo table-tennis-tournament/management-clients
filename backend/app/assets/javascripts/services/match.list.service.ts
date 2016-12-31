34fe145d97007dd611472a8b88059bcad1de8b2c
Dane <dane160290@yahoo.de>
2016-12-31 16:32:17 +0100

    matchlist delete and add with dragn drop

--- backend/app/assets/javascripts/services/match.list.service.ts
+++ backend/app/assets/javascripts/services/match.list.service.ts
@@ -8,6 +8,10 @@ import {Observable} from "rxjs/Rx";
 export class MatchListService {
     private nextMatchUrl = "matchlist/next";
     private allMatchListUrl = "matchlist/all";
+    private deleteMatchListItemUrl = "matchlist/deleteMatch/itemId ";
+    private addMatchListItemUrl = "matchlist/match/matchId/position ";
+    private deleteGroupListItemUrl = "matchlist/deleteGroup/itemId ";
+    private addGroupListItemUrl = "matchlist/deleteGroup/itemId ";
 
     constructor(private http: Http){}
 
@@ -19,5 +23,23 @@ export class MatchListService {
     getCompleteMatchlist(): Observable<MatchListDto[]>{
         return this.http.get(this.allMatchListUrl).map((res:Response) => res.json())
                .catch((error:any) => Observable.throw(error.json().error || "Server error"));
+    }
+
+    deleteMatchListItem(itemToDelete: MatchListDto): Observable<any>{
+        var regEx = new RegExp("itemId");
+        var url = this.deleteMatchListItemUrl.replace(regEx, itemToDelete.matchListItem.id.toString());
+        return this.http.delete(url).map((res:Response) => res.json())
+               .catch((error:any) => Observable.throw(error.json().error || "Server error"));
+    }
+
+    addMatchListItem(matchId: number, position: number): Observable<any>{
+        var headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        var regEx = new RegExp("matchId");
+        var url = this.addMatchListItemUrl.replace(regEx, matchId.toString());
+        regEx = new RegExp("position");
+        url = url.replace(regEx, position.toString());
+        return this.http.put(url, JSON.stringify(""), {headers: headers})
+             .map(res => res.json());
     }
 }
