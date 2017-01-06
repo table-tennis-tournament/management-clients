218b598a418b9fad5fac8261261831c8e8de8a27
dane <dane160290@yahoo.de>
2017-01-06 13:51:46 +0100

    add square for showing disciplines

--- app/assets/javascripts/components/discipline.group.view.component.ts
+++ app/assets/javascripts/components/discipline.group.view.component.ts
@@ -1,11 +1,19 @@
 import {Component, Input} from "@angular/core";
 import {DisciplineGroup} from "../data/discipline.group"
+import {TypeColors} from "../data/typeColors"
+
 
 @Component({
     selector: "group-view",
     templateUrl : "assets/javascripts/views/discipline.group.view.component.html"
 })
 export class DisciplineGroupViewComponent{
+
+    typeColors: string[];
+
+    constructor(){
+        this.typeColors = TypeColors.TYPE_COLORS;
+    }
 
      _group: DisciplineGroup;
     get group(): DisciplineGroup {
