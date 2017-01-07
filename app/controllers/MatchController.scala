decfd6cdface14bf52429370f61647184ebaf540
Jonas Hansert <hansert.jonas@gmail.com>
2017-01-07 21:17:33 +0100

    added matches by type

--- app/controllers/MatchController.scala
+++ app/controllers/MatchController.scala
@@ -47,6 +47,19 @@ class MatchController @Inject() (tables: Tables) extends Controller{
     }
   }
 
+  def getMatchesByType(typeId: Long) = {
+    val matchesF = tables.allMatches()
+    val x = matchesF map {matches =>
+      matches map(ttMatch => getAllMatchInfo(ttMatch))
+    }
+    val z = x map {y => Future.sequence(y)}
+    val z2 = z.flatMap(z => z)
+    z2 map {z =>
+      val m = z.filter(_.isDefined) map {z1 => z1.get}
+      Ok(Json.toJson(m.filter(_.ttMatch.typeId == typeId).sortBy(_.ttMatch.id)))
+    }
+  }
+
   def getAllMatches = Action.async {
     val matchesF = tables.allMatches()
     val x = matchesF map {matches =>
