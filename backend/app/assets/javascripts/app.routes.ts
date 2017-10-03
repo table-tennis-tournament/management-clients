0c496d25ba7aff1af29e2c9e217fc4014db86961
Dane <dane160290@yahoo.de>
2017-10-03 15:45:32 +0200

    add new view and first logic to assignment view

--- backend/app/assets/javascripts/app.routes.ts
+++ backend/app/assets/javascripts/app.routes.ts
@@ -4,11 +4,16 @@ import { Routes, RouterModule } from "@angular/router"
 import {PlayerComponent} from "./components/player.component"
 import {TableViewComponent} from "./components/table.view.component"
 import {SupervisorViewComponent} from "./components/supervisor.view.component"
+import {TableAssignViewComponent} from "./components/table.assign.view.component"
 
 export const routeConfig:Routes = [
     {
         path: "playerView",
         component: PlayerComponent
+    },
+    {
+        path: "assignView",
+        component: TableAssignViewComponent
     },
     {
         path: "tableView",
