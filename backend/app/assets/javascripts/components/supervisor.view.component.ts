75246dad544035dc701e756daba7ed0d2107539d
Dane <dane160290@yahoo.de>
2017-10-03 18:58:46 +0200

    remove some kinderdeseases

--- backend/app/assets/javascripts/components/supervisor.view.component.ts
+++ backend/app/assets/javascripts/components/supervisor.view.component.ts
@@ -3,12 +3,7 @@ import {MatchService} from "../services/match.service"
 import {TableService} from "../services/table.service"
 import {MatchToStringService} from "../services/match.toString.service"
 
-import {Table} from "../data/table"
-import {TableDto} from "../data/table.dto"
-import {Match} from "../data/match"
 
-import { Overlay } from "angular2-modal";
-import { Modal } from "angular2-modal/plugins/bootstrap";
 
 
 @Component({
@@ -16,7 +11,4 @@ import { Modal } from "angular2-modal/plugins/bootstrap";
 })
 export class SupervisorViewComponent{
 
-  constructor(){
-    console.log("start supervisor");
-  }
 }
