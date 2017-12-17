540de5d3c5bff1ff398c81b986d21eb74aa64dc0
Jonas Hansert <hansert.jonas@gmail.com>
2017-12-17 14:32:58 +0100

    print

--- app/actors/PrinterActor.scala
+++ app/actors/PrinterActor.scala
@@ -6,21 +6,21 @@ import javax.swing.JEditorPane
 import javax.swing.text.html.HTMLEditorKit
 
 import akka.actor._
-import models.TTMatch
-import play.api.Logger
+import models.{AllMatchInfo, TTMatch}
+import play.api.{Logger, Play}
 
 
 
 object PrinterActor {
   def props = Props[PrinterActor]
 
-  case class Print(name: String)
+  case class Print(allMatchInfo: AllMatchInfo)
   case class GetPrinter()
   case class SetPrinter(name: String)
 
-  sealed trait Answer
-  case object PrinterNotFound extends Answer
-  case object PrinterFound extends Answer
+  sealed trait PrinterAnswer
+  case object PrinterNotFound extends PrinterAnswer
+  case object PrinterFound extends PrinterAnswer
 }
 
 
@@ -29,14 +29,24 @@ class PrinterActor extends Actor {
   import actors.PrinterActor._
 
   var printService = PrintServiceLookup.lookupDefaultPrintService()
+  var html = scala.io.Source.fromFile("templates/print1.html").mkString
+  Logger.debug(html)
 
   def receive = {
-    case Print(name: String) =>
+    case Print(allMatchInfo) =>
+      val newHtml = html
+        .replaceAll("""\[name1\]""", allMatchInfo.player1.map(p => p.lastName + ", " + p.firstName).mkString(" / "))
+        .replaceAll("""\[name2\]""", allMatchInfo.player2.map(p => p.lastName + ", " + p.firstName).mkString(" / "))
+        .replaceAll("""\[club1\]""", allMatchInfo.player1.map(p => p.club.get.clubName).mkString(" / "))
+        .replaceAll("""\[club2\]""", allMatchInfo.player2.map(p => p.club.get.clubName).mkString(" / "))
+        .replaceAll("""\[id\]""", allMatchInfo.ttMatch.id.toString)
+        .replaceAll("""\[type\]""", allMatchInfo.ttType.name)
+        .replaceAll("""\[matchtype\]""", allMatchInfo.matchType.name)
       val editorPane = new JEditorPane()
       val htmlEditor = new HTMLEditorKit()
       editorPane.setEditorKit(htmlEditor)
       editorPane.setDocument(htmlEditor.createDefaultDocument())
-      editorPane.setText("<!DOCTYPE html>\n<html>\n<body>\n\n<h1>My First Heading</h1>\n\n<p>My first paragraph.</p>\n\n</body>\n</html>")
+      editorPane.setText(newHtml)
       editorPane.print(null, null, false, printService, null, false)
     case GetPrinter =>
       val printers = PrintServiceLookup.lookupPrintServices(null, null).map(p => p.getName).toSeq
--- app/controllers/PrinterController.scala
+++ app/controllers/PrinterController.scala
@@ -12,6 +12,7 @@ import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import akka.pattern.ask
 import akka.util.Timeout
+import dao.Tables
 import models.Answer
 
 import scala.concurrent.duration._
@@ -19,13 +20,20 @@ import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 import scala.concurrent.duration._
 
-class PrinterController @Inject() (@Named("printer_actor") printerActor: ActorRef) extends Controller{
+class PrinterController @Inject() (@Named("printer_actor") printerActor: ActorRef, tables: Tables) extends Controller{
   implicit val timeout: Timeout = 5.seconds
   import models.AnswerModel._
 
   def print(id: Long) = Action {
-    printerActor ! Print(id.toString)
-    Ok(Json.toJson(Answer(true, "printing")))
+    tables.getMatch(id) match {
+      case Some(ttMatch) => tables.getAllMatchInfo(ttMatch) match {
+        case Some(allMatchInfo) =>
+          printerActor ! Print(allMatchInfo)
+          Ok(Json.toJson(Answer(true, "printing")))
+        case _ => BadRequest(Json.toJson(Answer(false, "AllMatchInfo not found")))
+      }
+      case _ => BadRequest(Json.toJson(Answer(false, "Match not found")))
+    }
   }
 
   def allPrinter = Action.async {
@@ -33,7 +41,7 @@ class PrinterController @Inject() (@Named("printer_actor") printerActor: ActorRe
   }
 
   def setPrinter(name: String) = Action.async {
-    (printerActor ? SetPrinter(name)).mapTo[PrinterActor.Answer].map { res =>
+    (printerActor ? SetPrinter(name)).mapTo[PrinterActor.PrinterAnswer].map { res =>
       res match {
         case PrinterFound => Ok(Json.toJson(Answer(true, "Printer found")))
         case _ => BadRequest(Json.toJson(Answer(false, "Printer not found")))
