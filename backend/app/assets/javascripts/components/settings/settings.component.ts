0f3a94acc9d6f2c0eb3472be4c49bb7459f3fc16
Dane <dane160290@yahoo.de>
2017-12-24 17:26:16 +0100

    load settings complete, dont insert result on group, add fixed position for waiting list drop zone

--- backend/app/assets/javascripts/components/settings/settings.component.ts
+++ backend/app/assets/javascripts/components/settings/settings.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core"
 import { SettingsService } from "../../services/settings.service";
 import { StatusDto } from "../../data/status.dto";
 import { ToastService } from "../../services/toast.service";
+import { SettingDto } from "../../data/setting.dto";
 
 
 @Component({
@@ -11,17 +12,44 @@ export class SettingsComponent{
 
     public printers: string[];
     public selectedPrinter: string;
+    public isPrintForAssign: boolean;
 
     constructor(private settingsService:SettingsService, private toastService: ToastService) {
-        settingsService.getAllPrinters().subscribe(this.onPrinterLoaded.bind(this));
+        this.settingsService.getAllPrinters().subscribe(this.onPrinterLoaded.bind(this));
     }
 
     onPrinterLoaded(result:string[]){
         this.printers = result;
+        this.settingsService.getAllSettings().subscribe(this.onSettingsLoaded.bind(this));
+    }
+
+    onSettingsLoaded(settings: SettingDto[]){
+        var that = this;
+        settings.forEach(setting => {
+            if(setting.key === "printerName"){
+                that.selectedPrinter = setting.value;
+            }
+            if(setting.key === "alwaysPrint"){
+                that.isPrintForAssign = setting.value;
+            }
+        });
+        this.toastService.toast("Einstellungen geladen");
     }
 
     onSaveClicked(){
         this.settingsService.setPrinter(this.selectedPrinter).subscribe(this.onPrinterSet.bind(this));
+    }
+
+    onIsPrintForAssignChange(){
+        this.settingsService.setIsPrintForAssign(this.isPrintForAssign).subscribe(this.onSetPrintForAssignComplete.bind(this));
+    }
+
+    onSetPrintForAssignComplete(status: StatusDto){
+        if(status.successful){
+            this.toastService.toast("Wert gespeichert");
+            return;
+        }
+        this.toastService.toast(status.message);
     }
 
     onPrinterSet(status: StatusDto){
---
+++ backend/app/assets/javascripts/data/setting.dto.ts
@@ -0,0 +1,5 @@
+export class SettingDto{
+    key: string;
+    value: any;
+}
+
