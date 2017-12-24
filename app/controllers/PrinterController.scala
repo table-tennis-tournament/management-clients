de07177980ce50897daf0e6466bd5b63e5c34f8f
Jonas Hansert <hansert.jonas@gmail.com>
2017-12-24 17:28:39 +0100

    create schiri pdf

--- app/controllers/PrinterController.scala
+++ app/controllers/PrinterController.scala
@@ -13,6 +13,7 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import akka.pattern.ask
 import akka.util.Timeout
 import dao.Tables
+import it.innove.play.pdf.PdfGenerator
 import models.Answer
 
 import scala.concurrent.duration._
@@ -20,12 +21,13 @@ import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 import scala.concurrent.duration._
 
-class PrinterController @Inject() (@Named("printer_actor") printerActor: ActorRef, @Named("publisher_actor") pubActor: ActorRef, tables: Tables) extends Controller{
+class PrinterController @Inject() (pdfGenerator: PdfGenerator, @Named("printer_actor") printerActor: ActorRef, @Named("publisher_actor") pubActor: ActorRef, tables: Tables) extends Controller{
   implicit val timeout: Timeout = 5.seconds
   import models.AnswerModel._
 
   def print(id: Long) = Action {
-    tables.getMatch(id) match {
+    Ok(pdfGenerator.toBytes(views.html.schiri(tables.getAllMatchInfo(tables.getMatch(id).get).get), "http://localhost:9000/"))
+/*    tables.getMatch(id) match {
       case Some(ttMatch) => tables.getAllMatchInfo(ttMatch) match {
         case Some(allMatchInfo) =>
           printerActor ! Print(allMatchInfo)
@@ -33,7 +35,7 @@ class PrinterController @Inject() (@Named("printer_actor") printerActor: ActorRe
         case _ => BadRequest(Json.toJson(Answer(false, "AllMatchInfo not found")))
       }
       case _ => BadRequest(Json.toJson(Answer(false, "Match not found")))
-    }
+    }*/
   }
 
   def allPrinter = Action.async {
