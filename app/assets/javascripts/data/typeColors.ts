5421806bc54e00daab4bca958a287b6565e8f0ec
Dane <dane160290@yahoo.de>
2017-12-29 19:21:01 +0100

    change type colors, add number to schiri template

--- app/assets/javascripts/data/typeColors.ts
+++ app/assets/javascripts/data/typeColors.ts
@@ -4,43 +4,35 @@ export class TypeColors{
         TypeColors.TYPE_COLORS[0] = "brown darken-4";
         TypeColors.TYPE_COLORS[1] = "amber darken-1  white-text";
         TypeColors.TYPE_COLORS[2] = "orange";
-        TypeColors.TYPE_COLORS[3] = "light-green darken-1";
+        TypeColors.TYPE_COLORS[3] = "light-green darken-1 white-text";
         TypeColors.TYPE_COLORS[4] = "green";
-        TypeColors.TYPE_COLORS[5] = "red lighten-3";
+        TypeColors.TYPE_COLORS[5] = "red lighten-3 white-text";
         TypeColors.TYPE_COLORS[6] = "pink";
-        TypeColors.TYPE_COLORS[7] = "grey";
+        TypeColors.TYPE_COLORS[7] = "grey white-text";
         TypeColors.TYPE_COLORS[8] = "grey darken-3";
-        TypeColors.TYPE_COLORS[9] = "blue darken-1";
+        TypeColors.TYPE_COLORS[9] = "blue darken-1 white-text";
         TypeColors.TYPE_COLORS[10] = "indigo";
-        TypeColors.TYPE_COLORS[11] = "amber darken-1";
+        TypeColors.TYPE_COLORS[11] = "amber darken-1 white-text";
         TypeColors.TYPE_COLORS[12] = "orange";
-        TypeColors.TYPE_COLORS[13] = "light-green darken-1";
+        TypeColors.TYPE_COLORS[13] = "light-green darken-1 white-text";
         TypeColors.TYPE_COLORS[14] = "green";
-        TypeColors.TYPE_COLORS[15] = "red lighten-3";
+        TypeColors.TYPE_COLORS[15] = "red lighten-3 white-text";
         TypeColors.TYPE_COLORS[16] = "pink";
-        TypeColors.TYPE_COLORS[17] = "grey";
+        TypeColors.TYPE_COLORS[17] = "grey white-text";
         TypeColors.TYPE_COLORS[18] = "grey darken-3";
-        TypeColors.TYPE_COLORS[19] = "blue darken-1";
+        TypeColors.TYPE_COLORS[19] = "blue darken-1 white-text";
         TypeColors.TYPE_COLORS[20] = "indigo";
-        TypeColors.TYPE_COLORS[21] = "amber darken-4";
+        TypeColors.TYPE_COLORS[21] = "amber darken-4 white-text";
         TypeColors.TYPE_COLORS[22] = "orange";
-        TypeColors.TYPE_COLORS[23] = "light-green darken-1";
+        TypeColors.TYPE_COLORS[23] = "light-green darken-1 white-text";
         TypeColors.TYPE_COLORS[24] = "green";
-        TypeColors.TYPE_COLORS[25] = "red lighten-3";
+        TypeColors.TYPE_COLORS[25] = "red lighten-3 white-text";
         TypeColors.TYPE_COLORS[26] = "pink";
-        TypeColors.TYPE_COLORS[27] = "grey";
+        TypeColors.TYPE_COLORS[27] = "grey white-text";
         TypeColors.TYPE_COLORS[28] = "grey darken-3";
-        TypeColors.TYPE_COLORS[29] = "blue darken-1";
+        TypeColors.TYPE_COLORS[29] = "blue darken-1 white-text";
         TypeColors.TYPE_COLORS[30] = "indigo";
     }
     public static TYPE_COLORS = [
-        "grey",
-        "grey darken-3",
-        "amber darken-1",
-        "orange",
-        "red lighten-3",
-        "pink",
-        "blue darken-1",
-        "indigo"
     ]
 }
