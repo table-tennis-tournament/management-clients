62e771e455752a08b8f9df6e5b57252863b4fc84
Dane <dane160290@yahoo.de>
2018-01-01 20:21:41 +0100

    add table number: TODO:AddTableNumber

--- app/assets/javascripts/app.component.ts
+++ app/assets/javascripts/app.component.ts
@@ -10,7 +10,7 @@ import {DisciplineShortcuts} from "./data/disciplineShortcuts"
 export class AppComponent{
 
   constructor(private webSocketService: WebSocketService){
-    this.webSocketService.initializeWebSocket("ws://localhost:9000/register");
+    this.webSocketService.initializeWebSocket("ws://192.168.1.10:9000/register");
     new TypeColors();
     new DisciplineShortcuts();
   }
