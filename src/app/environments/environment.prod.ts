e6f14ea62e19904b10be9c4c105f219f641ea08a
Dane <dane160290@yahoo.de>
2018-12-23 12:27:10 +0100

    add deployment config and icons

--- src/app/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  socket: {
-    baseUrl: 'http://localhost:4200/register',
-    config: {}
-  }
-};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+    socket: {
+        baseUrl: 'ws://192.168.178.27:9000/api/register'
+    }
 };
