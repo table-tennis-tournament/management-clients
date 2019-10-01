cb5507e6e5734d34c6fab6fba1c5d5b292104917
Dane Leube <dane.leube@baloise.ch>
2019-10-01 14:39:35 +0200

    fix tests and travis build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +39,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('match-management-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('match-management-app app is running!');
-  });
 });
