772a9d71a3860c598e93a5f491b59ef2fa78377c
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-10-07 18:40:48 +0200

    Improve imports

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatMenuTrigger } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
