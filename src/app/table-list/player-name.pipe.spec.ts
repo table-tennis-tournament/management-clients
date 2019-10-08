53b104636ed716f318659d33f784bf511900bc90
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-10-08 20:58:00 +0200

    Delete unneded stuff

--- src/app/table-list/player-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlayerNamePipe } from './player-name.pipe';
-
-describe('PlayerNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlayerNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
