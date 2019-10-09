e3e2249903ac6d53784adf9588f3e9dd6f795ef9
luechtdiode <roland.seidel@interpolar.ch>
2019-10-09 10:58:44 +0200

    Integrate websocket via SocketJS (poc)

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,172 +1,139 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { Table } from '../table-list/tt-table/table.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export abstract class WebsocketService {
+export class WebsocketService {
 
-    private connectedState = false;
-    private websocket: WebSocket;
-    private backendUrl: string;
-    private explicitClosed = true;
+  websocket: any;
+  stompClient: any;
+  backendUrl: string;
 
-    connected = new BehaviorSubject<boolean>(false);
-    logMessages = new BehaviorSubject<string>('');
-    lastMessages: string[] = [];
+  connectedState = false;
+  connected = new BehaviorSubject<boolean>(false);
 
-    get stopped(): boolean {
-        return this.explicitClosed;
+  // this could be replaced by the redux-store implementation
+  activeTables = new BehaviorSubject<Table[]>([]);
+
+  logMessages = new BehaviorSubject<string>('');
+  lastMessages: string[] = [];
+
+  sendMessage(message) {
+    if (!this.websocket) {
+      this.connect();
+      this.stompClient.send('/topic/table', {}, JSON.stringify(message));
+    } else if (this.connectedState) {
+      this.stompClient.send('/topic/table', {}, JSON.stringify(message));
     }
+  }
 
-    sendMessage(message) {
-        if (!this.websocket) {
-            this.connect();
-            this.websocket.send(message);
-        } else if (this.connectedState) {
-            this.websocket.send(message);
+  disconnectWS() {
+    this.close();
+  }
+
+  private close() {
+    if (!!this.stompClient) {
+      this.stompClient.disconnect();
+    }
+    this.connectedState = false;
+    this.connected.next(this.connectedState);
+  }
+
+  protected getWebsocketBackendUrl(): string {
+    return '/api/ttt-management-websocket';
+  }
+
+  protected handleWebsocketMessage(message: any): boolean {
+    this.activeTables.next(message);
+    const type = message.type;
+    switch (type) {
+    //     case 'DurchgangStarted':
+    //       this._activeDurchgangList = [...this.activeDurchgangList, (message as DurchgangStarted)];
+    //       this.durchgangStarted.next(this.activeDurchgangList);
+    //       return true;
+
+    //     case 'DurchgangFinished':
+    //       const finished = (message as DurchgangFinished);
+    //       this._activeDurchgangList = this.activeDurchgangList
+    //         .filter(d => d.durchgang !== finished.durchgang || d.wettkampfUUID !== finished.wettkampfUUID);
+    //       this.durchgangStarted.next(this.activeDurchgangList);
+    //       return true;
+
+    //     case 'AthletWertungUpdatedSequenced':
+    //     case 'AthletWertungUpdated':
+    //       const updated = (message as AthletWertungUpdated);
+    //       this.wertungen = this.wertungen.map(w => {
+    //         if (w.id === updated.wertung.athletId && w.wertung.wettkampfdisziplinId === updated.wertung.wettkampfdisziplinId ) {
+    //           return Object.assign({}, w, {wertung: updated.wertung });
+    //         } else {
+    //           return w;
+    //         }
+    //       });
+    //       this.wertungenSubject.next(this.wertungen);
+    //       this.wertungUpdated.next(updated);
+    //       return true;
+
+    //     case 'AthletMovedInWettkampf':
+    //     case 'AthletRemovedFromWettkampf':
+    //       this.loadWertungen();
+    //       return true;
+
+    //     case 'NewLastResults':
+    //       this.newLastResults.next((message as NewLastResults));
+    //       return true;
+
+    //     case 'MessageAck':
+    //       console.log((message as MessageAck).msg);
+    //       this.showMessage.next((message as MessageAck));
+    //       return true;
+
+      default:
+        return false;
+    }
+  }
+
+  public initWebsocket() {
+    this.logMessages.subscribe(msg => {
+      this.lastMessages.push(msg);
+      this.lastMessages = this.lastMessages.slice(Math.max(this.lastMessages.length - 50, 0));
+    });
+    this.logMessages.next('init');
+    this.backendUrl = this.getWebsocketBackendUrl();
+    this.logMessages.next('init with ' + this.backendUrl);
+
+    this.connect();
+  }
+
+  private connect() {
+    this.disconnectWS();
+   
+    this.websocket = new SockJS(this.backendUrl);
+    this.stompClient = Stomp.over(this.websocket);
+    this.stompClient.connect({}, frame => {
+      this.connectedState = true;
+      this.stompClient.subscribe('/topic/table', evt => {
+        try {
+          const jsonMessage = JSON.parse(evt.body);
+          if (!this.handleWebsocketMessage(jsonMessage)) {
+            console.log(jsonMessage);
+            this.logMessages.next('unknown message: ' + evt.data);
+          }
+        } catch (e) {
+          this.logMessages.next(e + ': ' + evt.data);
         }
-    }
-
-    disconnectWS(explicit = true) {
-        this.explicitClosed = explicit;
-        if (this.websocket) {
-            this.websocket.close();
-            if (explicit) {
-                this.close();
-            }
-        } else {
-            this.close();
-        }
-    }
-
-    private close() {
-        if (this.websocket) {
-            this.websocket.onerror = undefined;
-            this.websocket.onclose = undefined;
-            this.websocket.onopen = undefined;
-            this.websocket.onmessage = undefined;
-            this.websocket.close();
-        }
-        this.websocket = undefined;
-        this.connectedState = false;
-        this.connected.next(this.connectedState);
-    }
-
-    public isWebsocketConnected(): boolean {
-        return this.websocket && this.websocket.readyState === this.websocket.OPEN;
-    }
-
-    private isWebsocketConnecting(): boolean {
-        return this.websocket && this.websocket.readyState === this.websocket.CONNECTING;
-    }
-
-    public shouldConnectAgain(): boolean {
-        return !(this.isWebsocketConnected() || this.isWebsocketConnecting());
-    }
-
-    protected getWebsocketBackendUrl(): string {
-        let host = location.host;
-        const path = '/'; // location.pathname;
-        const protocol = location.protocol === 'https:' ? 'wss:' : 'ws:';
-        const apiPath = 'api/tables/ws';
-
-        host = (protocol + '//' + host + path).replace('index.html', '');
-
-        return host + apiPath;
-    }
-
-    protected handleWebsocketMessage(message: any): boolean {
-        const type = message.type;
-        switch (type) {
-            //     case 'DurchgangStarted':
-            //       this._activeDurchgangList = [...this.activeDurchgangList, (message as DurchgangStarted)];
-            //       this.durchgangStarted.next(this.activeDurchgangList);
-            //       return true;
-
-            //     case 'DurchgangFinished':
-            //       const finished = (message as DurchgangFinished);
-            //       this._activeDurchgangList = this.activeDurchgangList
-            //         .filter(d => d.durchgang !== finished.durchgang || d.wettkampfUUID !== finished.wettkampfUUID);
-            //       this.durchgangStarted.next(this.activeDurchgangList);
-            //       return true;
-
-            //     case 'AthletWertungUpdatedSequenced':
-            //     case 'AthletWertungUpdated':
-            //       const updated = (message as AthletWertungUpdated);
-            //       this.wertungen = this.wertungen.map(w => {
-            //         if (w.id === updated.wertung.athletId && w.wertung.wettkampfdisziplinId === updated.wertung.wettkampfdisziplinId ) {
-            //           return Object.assign({}, w, {wertung: updated.wertung });
-            //         } else {
-            //           return w;
-            //         }
-            //       });
-            //       this.wertungenSubject.next(this.wertungen);
-            //       this.wertungUpdated.next(updated);
-            //       return true;
-
-            //     case 'AthletMovedInWettkampf':
-            //     case 'AthletRemovedFromWettkampf':
-            //       this.loadWertungen();
-            //       return true;
-
-            //     case 'NewLastResults':
-            //       this.newLastResults.next((message as NewLastResults));
-            //       return true;
-
-            //     case 'MessageAck':
-            //       console.log((message as MessageAck).msg);
-            //       this.showMessage.next((message as MessageAck));
-            //       return true;
-
-            default:
-                return false;
-        }
-    }
-
-    public initWebsocket() {
-        this.logMessages.subscribe(msg => {
-            this.lastMessages.push(msg);
-            this.lastMessages = this.lastMessages.slice(Math.max(this.lastMessages.length - 50, 0));
-        });
-        this.logMessages.next('init');
-        this.backendUrl = this.getWebsocketBackendUrl();
-        this.logMessages.next('init with ' + this.backendUrl);
-
+      });
+      //this.stompClient.reconnect_delay = 2000;
+    }, this.errorCallBack);
+  }
+  
+  errorCallBack(error) {
+    console.log("errorCallBack -> " + error)
+    setTimeout(() => {
         this.connect();
-    }
-
-    private connect() {
-        this.disconnectWS();
-        this.explicitClosed = false;
-        this.websocket = new WebSocket(this.backendUrl);
-        this.websocket.onopen = () => {
-            this.connectedState = true;
-            this.connected.next(this.connectedState);
-        };
-
-        this.websocket.onclose = (evt: CloseEvent) => {
-            this.close();
-        };
-
-        this.websocket.onmessage = (evt: MessageEvent) => {
-            try {
-                const jsonMessage = JSON.parse(evt.data);
-                const type = jsonMessage.type;
-                switch (type) {
-                    default:
-                        if (!this.handleWebsocketMessage(jsonMessage)) {
-                            console.log(jsonMessage);
-                            this.logMessages.next('unknown message: ' + evt.data);
-                        }
-                }
-            } catch (e) {
-                this.logMessages.next(e + ': ' + evt.data);
-            }
-        };
-
-        this.websocket.onerror = (e: ErrorEvent) => {
-            this.logMessages.next(e.message + ', ' + e.type);
-        };
-    }
+    }, 5000);
+  }
 }
--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -5,6 +5,7 @@ import * as MatchActions from './match/redux/match.actions';
 import * as TableActions from './redux/table-list.actions';
 import {AppState, getTables} from './redux/table-list.reducer';
 import {Table} from './tt-table/table.model';
+import { WebsocketService } from '../services/websocket.service';
 
 @Component({
     selector: 'app-table-list',
@@ -15,12 +16,16 @@ export class TableListComponent implements OnInit {
 
     tables$: Observable<Table[]>;
 
-    constructor(private store: Store<AppState>) {
+    eventmessages: any;
+
+    constructor(private store: Store<AppState>, private events: WebsocketService) {
         this.tables$ = store.pipe(select(getTables));
     }
 
     ngOnInit() {
         this.store.dispatch(TableActions.loadTables({tableManagerId: 1}));
+        this.events.activeTables.subscribe(tables => this.eventmessages = tables);
+        this.events.initWebsocket();
     }
 
     onUpdateMatchResult($event: any) {
