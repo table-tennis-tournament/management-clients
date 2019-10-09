37ce2f80e092c19005bf6635482c0ce3b13bb080
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-10-09 14:22:33 +0200

    Add error handling for matches

--- src/app/table-list/match/redux/match.effects.ts
+++ src/app/table-list/match/redux/match.effects.ts
@@ -1,7 +1,9 @@
 import {Injectable} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {of} from 'rxjs';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError, map, switchMap, tap} from 'rxjs/operators';
+import * as TableActions from '../../redux/table-list.actions';
 import {MatchService} from '../match.service';
 import * as MatchActions from './match.actions';
 
@@ -18,8 +20,16 @@ export class MatchEffects {
         )
     ));
 
+    updateMatchResultError$ = createEffect(() => this.actions$.pipe(
+        ofType(TableActions.loadTablesError),
+        tap(() => {
+            this.snackBar.open('Match could not be updated.');
+        })
+    ), {dispatch: false});
+
     constructor(
         private actions$: Actions,
+        private snackBar: MatSnackBar,
         private matchService: MatchService) {
     }
 }
