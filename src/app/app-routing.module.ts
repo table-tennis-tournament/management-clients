1ee533c67750ce25aeff30675f5d432619c2b832
Christian Siegel <christian.siegel@baloise.ch>
2019-10-09 18:40:42 +0200

    Use hash routing
    This way we don't need to fiddle around with the nginx config ;D

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes),
+        RouterModule.forRoot(routes, {useHash: true}),
         TableListModule
     ],
     exports: [RouterModule]
