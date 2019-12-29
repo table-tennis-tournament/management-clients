799087b0d83a718ee1a83b69d6fc81b4d76d1f35
Dane <dane160290@yahoo.de>
2019-12-29 22:43:53 +0100

    add filter player possibility

--- src/app/player/player-list/player-list.component.ts
+++ src/app/player/player-list/player-list.component.ts
@@ -8,13 +8,48 @@ import {Player} from '../player.model';
 })
 export class PlayerListComponent {
 
-    @Input()
-    players: Player[];
+    _players: Player[];
+
+    get players(): Player[] {
+        return this._players;
+    }
+
+    @Input('players')
+    set players(players: Player[]) {
+        this._players = players;
+        this.filteredPlayers = players;
+    }
+
+    filteredPlayers: Player[];
 
     @Input()
     playersLoading: boolean;
 
+    firstNameInput: string;
+    clubInput: string;
+    lastNameInput: string;
+
     constructor() {
     }
 
+    filterPlayers(value: string, callback: (player: Player) => string) {
+        this.filteredPlayers = this.players
+            .filter(player => this.containsValue(callback(player), value));
+    }
+
+    filterByFirstName(value: string) {
+        this.filterPlayers(value, p => p.firstName);
+    }
+
+    private containsValue(value: string, input: string) {
+        return value.toLowerCase().indexOf(input.toLowerCase()) > -1;
+    }
+
+    filterByLastName(value: string) {
+        this.filterPlayers(value, p => p.lastName);
+    }
+
+    filterByClub(value: string) {
+        this.filterPlayers(value, p => p.club.clubName);
+    }
 }
