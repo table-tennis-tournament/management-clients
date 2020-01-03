b8d45bbcdb55523d38efcc94583c88a0e8691327
dane <dane160290@yahoo.de>
2020-01-03 01:10:49 +0100

    add waiting time to second call

--- src/app/caller/player-calls/player-calls.component.ts
+++ src/app/caller/player-calls/player-calls.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {MatchAggregate} from '../../shared/data/match.aggregate';
+import * as moment from 'moment';
 
 @Component({
   selector: 'toma-player-calls',
@@ -23,4 +24,10 @@ export class PlayerCallsComponent {
   constructor() { }
 
 
+  getStartTime(currentMatch: MatchAggregate) {
+      const date = moment(currentMatch.startTime, 'DD/MM/YYYY HH:mm:ss').add(1, 'hours');
+      const now = moment().add(1, 'hours');
+      const duration = moment.duration(now.diff(date));
+      return Math.floor(duration.asMinutes());
+    }
 }
