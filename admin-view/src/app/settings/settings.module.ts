9b4247f86fa04173fb5460fe3f3ecca5786cdd2d
leubedane <dane160290@yahoo.de>
2024-12-14 16:59:01 +0100

    feat: Disable scanner to continue update

--- admin-view/src/app/settings/settings.module.ts
+++ admin-view/src/app/settings/settings.module.ts
@@ -3,13 +3,12 @@ import { SharedModule } from '../shared/shared.module';
 import { SettingsPageComponent } from './settings.page.component';
 import { SettingsListComponent } from './settings-list/settings-list.component';
 import { QrResultScannerComponent } from './qr-result-scanner/qr-result-scanner.component';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 const components = [SettingsPageComponent, SettingsListComponent, QrResultScannerComponent];
 
 @NgModule({
   declarations: [...components],
-  imports: [SharedModule, ZXingScannerModule.forRoot()],
+  imports: [SharedModule],
   providers: [],
   exports: [SettingsPageComponent],
 })
